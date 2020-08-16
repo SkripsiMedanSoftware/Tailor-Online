0d1e483caa7e8c16803d7e4dc1f634339bcb56af
Agung Dirgantara <agungmasda29@gmail.com>
2020-08-16 07:34:32 +0700

    Double close php

--- application/views/admin/base.php
+++ application/views/admin/base.php
@@ -51,12 +51,12 @@
 					</li>
 					<li class="dropdown user user-menu">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-							<img src="<?php echo (!empty(aktif_sesi()['foto']))?base_url(aktif_sesi()['foto']):base_url('assets/adminlte-2.4.8/dist/img/user2-160x160.jpg') ?> ?>" class="user-image" alt="User Image">
+							<img src="<?php echo (!empty(aktif_sesi()['foto']))?base_url(aktif_sesi()['foto']):base_url('assets/adminlte-2.4.8/dist/img/user2-160x160.jpg') ?>" class="user-image" alt="User Image">
 							<span class="hidden-xs"><?php echo aktif_sesi()['nama_lengkap'] ?></span>
 						</a>
 						<ul class="dropdown-menu">
 							<li class="user-header">
-								<img src="<?php echo (!empty(aktif_sesi()['foto']))?base_url(aktif_sesi()['foto']):base_url('assets/adminlte-2.4.8/dist/img/user2-160x160.jpg') ?> ?>" class="img-circle" alt="User Image">
+								<img src="<?php echo (!empty(aktif_sesi()['foto']))?base_url(aktif_sesi()['foto']):base_url('assets/adminlte-2.4.8/dist/img/user2-160x160.jpg') ?>" class="img-circle" alt="User Image">
 								<p><?php echo aktif_sesi()['nama_lengkap'] ?></p>
 							</li>
 							<li class="user-footer">
@@ -77,7 +77,7 @@
 		<section class="sidebar">
 			<div class="user-panel">
 				<div class="pull-left image">
-					<img src="<?php echo (!empty(aktif_sesi()['foto']))?base_url(aktif_sesi()['foto']):base_url('assets/adminlte-2.4.8/dist/img/user2-160x160.jpg') ?> ?>" class="img-circle" alt="User Image">
+					<img src="<?php echo (!empty(aktif_sesi()['foto']))?base_url(aktif_sesi()['foto']):base_url('assets/adminlte-2.4.8/dist/img/user2-160x160.jpg') ?>" class="img-circle" alt="User Image">
 				</div>
 				<div class="pull-left info">
 					<p><?php echo aktif_sesi()['nama_lengkap'] ?></p>
